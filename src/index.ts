6f35e5baf54b4ce570ecddaf3114a892b1563b23
grim <75869731+vys69@users.noreply.github.com>
2024-10-16 19:42:38 -0700

    grrrrr

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import router from './routes';
+import routes from './routes';
 
 const app = express();
 const port = process.env.PORT || 6969;
 
-app.use('/', router);
+app.use('/', routes);
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
