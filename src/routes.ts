6191dcbe794507bff6cddff9c563c5976730b0be
grim <75869731+vys69@users.noreply.github.com>
2024-10-31 00:15:20 -0700

    pls work or ill harm my whole family

--- src/routes.ts
+++ src/routes.ts
@@ -312,7 +312,7 @@ router.get('/owoembed', (req, res) => {
   const oembedResponse = {
       author_name: decodeURIComponent(text as string),
       author_url: `https://www.roblox.com/users/${status}/profile`,
-      provider_name: "FixRoblox",
+      provider_name: "FixRoblox / RxBlox",
       provider_url: "https://fixroblox.com",
       title: `${author}'s Roblox Profile`,
       type: "link",
